74cebf77f6bcad0a3f990c9378af771ad667612a
shashwata <shashwatahalder01@gmail.com>
2022-08-24 15:42:25 +0600

    fixed expo test issues

--- tests/e2e/customSequencer.js
+++ tests/e2e/customSequencer.js
@@ -6,8 +6,8 @@ class CustomSequencer extends Sequencer {
     const copyTests = Array.from(tests)
 
     const setupTests = copyTests.filter(test => test.path.includes('setup'))
-    const exploratoryTests = copyTests.filter(test => test.path.includes('exploratory'))
-    const actualTests = copyTests.filter(test => !test.path.includes('setup') && !test.path.includes('exploratory'))
+    const exploratoryTests = copyTests.filter(test => test.path.includes('zexploratory'))
+    const actualTests = copyTests.filter(test => !test.path.includes('setup') && !test.path.includes('zexploratory'))
     actualTests.sort((testA, testB) => (testA.path > testB.path ? 1 : -1))
     // console.log(setupTests, exploratoryTests, actualTests)
     const sortedTests = [...setupTests, ...actualTests, ...exploratoryTests]
