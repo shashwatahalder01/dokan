5181c3fc76003d4344f79fc66c59e5cde7682f41
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-06-27 01:31:37 +0600

    add tests rfq

--- tests/pw/a.mjs
+++ tests/pw/a.mjs
@@ -5,7 +5,7 @@ const file = createWriteStream('file.jpg');
 const request = get('http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg', function(response) {
 	response.pipe(file);
 
-	// after download completed close filestream
+	// after download completed close filestreams
 	file.on('finish', () => {
 		file.close();
 		console.log('Download Completed');
