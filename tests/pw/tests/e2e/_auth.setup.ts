f029a4b2be85ecb29cff8ba2d790d9f9e3f274a8
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-07-13 13:21:54 +0600

    update yml

--- tests/pw/tests/e2e/_auth.setup.ts
+++ tests/pw/tests/e2e/_auth.setup.ts
@@ -7,41 +7,41 @@ import { payloads } from 'utils/payloads';
 
 setup.describe('authenticate users & set permalink', () => {
 
-	setup('authenticate admin', async ({ page }) => {
+	setup('authenticate admin @lite @pro', async ({ page }) => {
 		const loginPage = new LoginPage(page);
 		await loginPage.adminLogin(data.admin, data.auth.adminAuthFile);
 	});
 
-	setup('admin set WpSettings', async ({ page }) => {
+	setup('admin set WpSettings @lite @pro', async ({ page }) => {
 		const loginPage = new LoginPage(page);
 		const wpPage = new WpPage(page);
 		await loginPage.adminLogin(data.admin);
 		await wpPage.setPermalinkSettings(data.wpSettings.permalink);
 	});
 
-	setup('add customer', async ({ request }) => {
+	setup('add customer @lite @pro', async ({ request }) => {
 		const apiUtils = new ApiUtils(request);
 		const [, customerId] = await apiUtils.createCustomer (payloads.createCustomer1, payloads.adminAuth);
 		process.env.CUSTOMER_ID = customerId;
 	});
 
-	setup('add vendor', async ({ request }) => {
+	setup('add vendor @lite @pro', async ({ request }) => {
 		const apiUtils = new ApiUtils(request);
 		const [, sellerId] = await apiUtils.createStore (payloads.createStore1, payloads.adminAuth);
 		process.env.VENDOR_ID = sellerId;
 	});
 
-	setup('authenticate customer', async ({ page }) => {
+	setup('authenticate customer @lite @pro', async ({ page }) => {
 		const loginPage = new LoginPage(page);
 		await loginPage.login(data.customer, data.auth.customerAuthFile);
 	});
 
-	setup('authenticate vendor', async ({ page }) => {
+	setup('authenticate vendor @lite @pro', async ({ page }) => {
 		const loginPage = new LoginPage(page);
 		await loginPage.login(data.vendor, data.auth.vendorAuthFile);
 	});
 
-	setup('Dokan Pro enabled or not ', async ({ request }) => {
+	setup('Dokan Pro enabled or not @lite @pro', async ({ request }) => {
 		const apiUtils = new ApiUtils(request);
 		const res = await apiUtils.pluginsActiveOrNot(data.plugin.dokanPro, payloads.adminAuth);
 		process.env.DOKAN_PRO = String(res);
