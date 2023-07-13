baaa1d3282c7db3c5cc51d578fdc9373984cf416
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-07-13 22:10:06 +0600

    update test tag

--- tests/pw/tests/e2e/customer.spec.ts
+++ tests/pw/tests/e2e/customer.spec.ts
@@ -21,20 +21,20 @@ test.describe('Customer user functionality test', () => {
 		await page.close();
 	});
 
-	test('customer can register', async () => {
+	test('customer can register @lite @pro', async () => {
 		await customerPage.customerRegister(data.customer.customerInfo);
 	});
 
-	test('customer can login', async () => {
+	test('customer can login @lite @pro', async () => {
 		await loginPage.login(data.customer);
 	});
 
-	test('customer can logout', async () => {
+	test('customer can logout @lite @pro', async () => {
 		await loginPage.login(data.customer);
 		await loginPage.logout();
 	});
 
-	test('customer can become a vendor', async () => {
+	test('customer can become a vendor @lite @pro', async () => {
 		await customerPage.customerRegister(data.customer.customerInfo);
 		await customerPage.customerBecomeVendor(data.customer.customerInfo);
 	});
@@ -58,55 +58,55 @@ test.describe('Customer functionality test', () => {
 		await page.close();
 	});
 
-	test('customer can add billing details', async ( ) => {
+	test('customer can add billing details @lite @pro', async ( ) => {
 		await customerPage.addBillingAddress(data.customer.customerInfo);
 	});
 
-	test('customer can add shipping details', async ( ) => {
+	test('customer can add shipping details @lite @pro', async ( ) => {
 		await customerPage.addShippingAddress(data.customer.customerInfo);
 	});
 
-	test('customer can add customer details', async ( ) => {
+	test('customer can add customer details @lite @pro', async ( ) => {
 		await customerPage.addCustomerDetails(data.customer.customerInfo);
 	});
 
 
-	test('customer can search product @product', async ( ) => {
+	test('customer can search product @lite @pro', async ( ) => {
 		await customerPage.searchProduct(data.predefined.simpleProduct.product1.name);
 	});
 
-	test('customer can review product', async ( ) => {
+	test('customer can review product @lite @pro', async ( ) => {
 		await customerPage.reviewProduct(data.predefined.simpleProduct.product1.name, data.product.review);
 	});
 
-	test('customer can report product', async ( ) => {
+	test('customer can report product @lite @pro', async ( ) => {
 		await customerPage.reportProduct(data.predefined.simpleProduct.product1.name, data.product.report);
 	});
 
-	test('customer can enquire product', async ( ) => {
+	test('customer can enquire product @lite @pro', async ( ) => {
 		await customerPage.enquireProduct(data.predefined.simpleProduct.product1.name, data.product.enquiry);
 	});
 
-	test('customer can buy product', async ( ) => {
+	test('customer can buy product @lite @pro', async ( ) => {
 		await customerPage.clearCart();
 		await customerPage.addProductToCartFromSingleProductPage(data.predefined.simpleProduct.product1.name);
 		await customerPage.placeOrder();
 	});
 
-	test('customer can add product to cart', async ( ) => {
+	test('customer can add product to cart @lite @pro', async ( ) => {
 		await customerPage.clearCart();
 		await customerPage.addProductToCartFromSingleProductPage(data.predefined.simpleProduct.product1.name);
 		await customerPage.goToCartFromSingleProductPage();
 	});
 
-	test('customer can apply coupon', async ( ) => {
+	test('customer can apply coupon @pro', async ( ) => {
 		await customerPage.clearCart();
 		await customerPage.addProductToCartFromSingleProductPage(data.predefined.simpleProduct.product1.name);
 		await customerPage.goToCartFromSingleProductPage();
 		await customerPage.applyCoupon(data.predefined.coupon.couponCode);
 	});
 
-	test('customer can search store', async ( ) => {
+	test('customer can search store @lite @pro', async ( ) => {
 		await customerPage.searchStore(data.predefined.vendorStores.vendor1); //TODO: update parameter
 	});
 
@@ -114,15 +114,15 @@ test.describe('Customer functionality test', () => {
 		await customerPage.followStore(data.predefined.vendorStores.vendor1, data.predefined.vendorStores.followFromShopPage); //TODO: update parameter
 	});
 
-	test('customer can review store', async ( ) => {
+	test('customer can review store @pro', async ( ) => {
 		await customerPage.reviewStore(data.predefined.vendorStores.vendor1, data.store);
 	});
 
-	test('customer can ask for get support ', async ( ) => {
+	test('customer can ask for get support @pro', async ( ) => {
 		await customerPage.askForGetSupport(data.predefined.vendorStores.vendor1, data.customer.customerInfo.getSupport);
 	});
 
-	test('customer can send message to support ticket', async ( ) => {
+	test('customer can send message to support ticket @pro', async ( ) => {
 		await customerPage.askForGetSupport(data.predefined.vendorStores.vendor1, data.customer.customerInfo.getSupport);
 		await customerPage.sendMessageCustomerSupportTicket(data.customer.supportTicket);
 	});
--- tests/pw/tests/e2e/vendor.spec.ts
+++ tests/pw/tests/e2e/vendor.spec.ts
@@ -22,16 +22,16 @@ test.describe('Vendor user functionality test1', () => {
 		await page.close();
 	});
 
-	test('vendor can register', async ( ) => {
+	test('vendor can register @lite @pro', async ( ) => {
 		await vendorPage.vendorRegister(data.vendor.vendorInfo, { ...data.vendorSetupWizard, choice:false });
 		// await loginPage.logout();
 	});
 
-	test('vendor can login', async ( ) => {
+	test('vendor can login @lite @pro', async ( ) => {
 		await loginPage.login(data.vendor);
 	});
 
-	test('vendor can logout', async ( ) => {
+	test('vendor can logout @lite @pro', async ( ) => {
 		await loginPage.login(data.vendor);
 		await loginPage.logout();
 	});
@@ -55,56 +55,56 @@ test.describe('Vendor functionality test', () => {
 		await page.close();
 	});
 
-	test('vendor can setup setup-wizard', async ( ) => {
+	test('vendor can setup setup-wizard @lite @pro', async ( ) => {
 		await vendorPage.vendorSetupWizard(data.vendorSetupWizard);
 	});
 
-	test('vendor can add simple product @product', async ( ) => {
+	test('vendor can add simple product @lite @pro', async ( ) => {
 		await vendorPage.addSimpleProduct(data.product.simple);
 	});
 
-	test.fixme('vendor can add variable product', async ( ) => {
+	test.fixme('vendor can add variable product @pro', async ( ) => {
 		await vendorPage.addVariableProduct(data.product.variable);
 	});
 
-	test('vendor can add simple subscription product', async ( ) => {
+	test('vendor can add simple subscription product @pro', async ( ) => {
 		await vendorPage.addSimpleSubscription(data.product.simpleSubscription);
 	});
 
-	test.fixme('vendor can add variable subscription product', async ( ) => {
+	test.fixme('vendor can add variable subscription product @pro', async ( ) => {
 		await vendorPage.addVariableSubscription(data.product.variableSubscription);
 	});
 
-	test('vendor can add external product', async ( ) => {
+	test('vendor can add external product @pro', async ( ) => {
 		await vendorPage.addExternalProduct(data.product.external);
 	});
 
-	test('vendor can add auction product', async ( ) => {
+	test('vendor can add auction product @pro', async ( ) => {
 		await vendorPage.addAuctionProduct(data.product.auction);
 	});
 
-	test('vendor can add booking product', async ( ) => {
+	test('vendor can add booking product @pro', async ( ) => {
 		await vendorPage.addBookingProduct(data.product.booking);
 	});
 
 
-	test('vendor can add payment method', async ( ) => {
+	test('vendor can add payment method @lite @pro', async ( ) => {
 		await vendorPage.setPaymentSettings(data.vendor.payment);
 	});
 
-	test('vendor can request withdraw', async ( ) => {
+	test('vendor can request withdraw @lite @pro', async ( ) => {
 		await vendorPage.requestWithdraw(data.vendor.withdraw);
 	});
 
-	test('vendor can cancel request withdraw', async ( ) => {
+	test('vendor can cancel request withdraw @lite @pro', async ( ) => {
 		await vendorPage.cancelRequestWithdraw( data.vendor.withdraw );
 	});
 
-	test.skip('vendor can add auto withdraw disbursement schedule', async ( ) => {
+	test.skip('vendor can add auto withdraw disbursement schedule @pro', async ( ) => {
 		await vendorPage.addAutoWithdrawDisbursementSchedule(data.vendor.withdraw);
 	});
 
-	test('vendor can add default withdraw payment methods ', async ( ) => {
+	test('vendor can add default withdraw payment methods @lite @pro', async ( ) => {
 		await vendorPage.addDefaultWithdrawPaymentMethods(data.vendor.withdraw.defaultWithdrawMethod.bankTransfer);
 		// Cleanup
 		await vendorPage.addDefaultWithdrawPaymentMethods(data.vendor.withdraw.defaultWithdrawMethod.paypal);
@@ -116,64 +116,64 @@ test.describe('Vendor functionality test', () => {
 	});
 
 	// store settings
-	test('vendor can set store settings ', async ( ) => {
+	test('vendor can set store settings @lite @pro', async ( ) => {
 		await vendorPage.setStoreSettings(data.vendor.vendorInfo);
 	});
 
-	test('vendor can add addons', async ( ) => {
+	test('vendor can add addons @pro', async ( ) => {
 		await vendorPage.addAddon(data.vendor.addon);
 	});
 
-	test.fixme('vendor can edit addon', async ( ) => {
+	test.fixme('vendor can edit addon @pro', async ( ) => {
 		const addonName = await vendorPage.addAddon(data.vendor.addon);
 		await vendorPage.editAddon(data.vendor.addon, addonName);
 	});
 
-	// test.skip('vendor can send id verification request ', async ( )=> {
+	// test.skip('vendor can send id verification request @pro', async ( )=> {
 	// 	await vendorPage.sendIdVerificationRequest(data.vendor.verification);
 	// });
 
-	// test.skip('vendor can send address verification request ', async ( )=> {
+	// test.skip('vendor can send address verification request @pro', async ( )=> {
 	// 	await vendorPage.sendAddressVerificationRequest(data.vendor.verification);
 	// });
 
-	// test.skip('vendor can send company verification request ', async ( )=> {
+	// test.skip('vendor can send company verification request @pro', async ( )=> {
 	// 	await vendorPage.sendCompanyVerificationRequest(data.vendor.verification);
 	// });
 
-	// test.skip('vendor can set delivery time settings ', async ( )=> {
+	// test.skip('vendor can set delivery time settings @pro', async ( )=> {
 	// 	await vendorPage.setDeliveryTimeSettings(data.vendor.deliveryTime);
 	// });
 
-	test('vendor can set shipping policy', async ( ) => {
+	test('vendor can set shipping policy @pro', async ( ) => {
 		await vendorPage.setShippingPolicies(data.vendor.shipping.shippingPolicy);
 	});
 
-	test('vendor can set flat rate shipping ', async ( ) => {
+	test('vendor can set flat rate shipping @pro', async ( ) => {
 		await vendorPage.setShippingSettings(data.vendor.shipping.shippingMethods.flatRate);
 	});
 
-	test('vendor can set free shipping ', async ( ) => {
+	test('vendor can set free shipping @pro', async ( ) => {
 		await vendorPage.setShippingSettings(data.vendor.shipping.shippingMethods.freeShipping);
 	});
 
-	test('vendor can set local pickup shipping ', async ( ) => {
+	test('vendor can set local pickup shipping @pro', async ( ) => {
 		await vendorPage.setShippingSettings(data.vendor.shipping.shippingMethods.localPickup);
 	});
 
-	test('vendor can set table rate shipping shipping ', async ( ) => {
+	test('vendor can set table rate shipping shipping @pro', async ( ) => {
 		await vendorPage.setShippingSettings(data.vendor.shipping.shippingMethods.tableRateShipping);
 	});
 
-	test('vendor can set dokan distance rate shipping ', async ( ) => {
+	test('vendor can set dokan distance rate shipping @pro', async ( ) => {
 		await vendorPage.setShippingSettings(data.vendor.shipping.shippingMethods.distanceRateShipping);
 	});
 
-	test('vendor can set social profile settings ', async ( ) => {
+	test('vendor can set social profile settings @pro', async ( ) => {
 		await vendorPage.setSocialProfile(data.vendor.socialProfileUrls);
 	});
 
-	test('vendor can set rma settings ', async ( ) => {
+	test('vendor can set rma settings @pro', async ( ) => {
 		await vendorPage.setRmaSettings(data.vendor.rma);
 	});
 
