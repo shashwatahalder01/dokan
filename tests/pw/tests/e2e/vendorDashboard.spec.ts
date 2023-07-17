5d6cc99cbf4143769612f13a268e68cab9992a29
shashwatahalder01 <shashwatahalder01@gmail.com>
2023-07-17 20:25:25 +0600

    update single product tests

--- tests/pw/tests/e2e/vendorDashboard.spec.ts
+++ tests/pw/tests/e2e/vendorDashboard.spec.ts
@@ -1,5 +1,5 @@
 import { test, Page } from '@playwright/test';
-import { AdminDashboardPage } from 'pages/adminDashboardPage';
+import { VendorDashboardPage } from 'pages/vendorDashboardPage';
 import { ApiUtils } from 'utils/apiUtils';
 import { data } from 'utils/testData';
 import { payloads } from 'utils/payloads';
@@ -8,34 +8,26 @@ import { payloads } from 'utils/payloads';
 test.describe('Admin dashboard test', () => {
 
 
-	let adminDashboardPage: AdminDashboardPage;
-	let page: Page;
+	let vendorDashboardPage: VendorDashboardPage;
+	let vPage: Page;
 	let apiUtils: ApiUtils;
 
 	test.beforeAll(async ({ browser, request }) => {
-		const adminContext = await browser.newContext({ storageState: data.auth.adminAuthFile });
-		page = await adminContext.newPage();
-		adminDashboardPage = new AdminDashboardPage(page);
+		const adminContext = await browser.newContext({ storageState: data.auth.vendorAuthFile });
+		vPage = await adminContext.newPage();
+		vendorDashboardPage = new VendorDashboardPage(vPage);
 		apiUtils = new ApiUtils(request);
 	});
 
 	test.afterAll(async ( ) => {
-		await page.close();
+		await vPage.close();
 	});
 
 	// test.use({ storageState: data.auth.adminAuthFile });
 
-	test('dokan admin dashboard is rendering properly @lite @pro @explo', async ( ) => {
-		await adminDashboardPage.adminDashboardRenderProperly();
+	test.skip('vendor dashboard is rendering properly @lite @pro @explo', async ( ) => {
+		await vendorDashboardPage.vendorDashboardRenderProperly();
 	});
 
-	test('admin dashboard at a glance values are accurate @lite @pro', async ( ) => {
-		const summary = await apiUtils.getAdminReportSummary( payloads.adminAuth); //TODO: fix admin auth don't work if test use auth from storage json
-		await adminDashboardPage.dokanAtAGlanceValueAccuracy(summary);
-	});
-
-	test('admin can add dokan news subscriber @lite @pro', async ( ) => {
-		await adminDashboardPage.addDokanNewsSubscriber(data.user.userDetails);
-	});
 
 });
