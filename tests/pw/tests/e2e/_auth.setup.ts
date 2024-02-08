f3adfa9fdb586b5d385317e8fc0a4cfbe1c9c2f0
shashwata <shashwatahalder01@gmail.com>
2024-02-09 00:12:43 +0600

    removed unused variable

--- tests/pw/tests/e2e/_auth.setup.ts
+++ tests/pw/tests/e2e/_auth.setup.ts
@@ -51,7 +51,7 @@ setup.describe('authenticate users & set permalink', () => {
     });
 
     setup('add vendor1 @lite', async () => {
-        const [responseBody, sellerId] = await apiUtils.createStore(payloads.createStore1, payloads.adminAuth);
+        const [, sellerId] = await apiUtils.createStore(payloads.createStore1, payloads.adminAuth);
         await apiUtils.updateCustomer(sellerId, payloads.updateAddress, payloads.adminAuth);
         console.log('VENDOR_ID:', sellerId);
         process.env.VENDOR_ID = sellerId;
