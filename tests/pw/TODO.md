5c8a42da6857cdb9445e0f8e165fa75ceb2f8473
shashwata <shashwatahalder01@gmail.com>
2024-02-17 14:08:46 +0600

    updated suite

--- tests/pw/TODO.md
+++ tests/pw/TODO.md
@@ -1,51 +1,73 @@
 <!-- prettier-ignore -->
-
 ### Todo: E2E-API TaskList
 
-- [ ] Project: add more comments for code readability
-- [ ] Project: add JSDOC  
-- [ ] Project: add parallelism for both git action & playwright tests
-- [ ] Project: GRAB CONSOLE ERROR, And PHP ERROR  
-- [ ] TestData: update all image path from project root   
-- [ ] Zip_suite: add zip testing tests  
-- [ ] Selectors: add html tag to every css locator e.g. #id to input#id  
-- [ ] Selectors: shorten selector is all pages e.g. adminSettings = selector.admin.dokan.settings; 
-- [ ] Reporter: add os and browsers in env info  
-- [ ] Reporter: convert testSummary from commonjs module to es-module  
-- [ ] Readme: add readme contribute to e2e_api project  
-- [ ] Readme: rephrase readme  
-- [ ] Readme: fix bookmark issue
-- [ ] Readme: update readme following readme guideline  
-- [ ] E2E_Suite: List all core tests and setup tests : plugins activated, wp settings, woocommerce settings, dokan modules activated , dokan settings for both e2e and api  
-- [ ] E2E_Suite: Slack integration  
-- [ ] E2E_Suite: Working-directory: ./path/to/tests  
-- [ ] E2E_Suite: Update auth if expired instead of every-time  
-- [ ] E2E_Suite: Report: separate two junit report showing on simple git-action summary  
-- [ ] E2E_Suite: Global setup & teardown can be converted to project setup: no need  
-- [ ] E2E_Suite: Project Configuration: separate everything for local & CI like: global setup, env, playwright config, Configure projects for multiple environments : ci, local,  
-- [ ] E2E_Suite: Fixture: add fixture: separate user role, implement fixture for lite pro issue handle    
-- [ ] E2E_Suite: Test basic auth can be used instead of cookies for authentication , no need to do this  
-- [ ] E2E_Suite: why two pages are opening : fix that    
-- [ ] E2E_Suite: Modal: Close modal if exists, causes issue with other tests where popup blocks next step*****  
-- [ ] E2E_Suite: add new tests or multiple entry in same test for all search parameter, i.e : by-customer, by-product, but-store,..  
-- [ ] E2E_Suite: insert all date format according to site format , currently hardcoded to default format -> helpers.dateFormatFYJ  
-- [ ] E2E_Suite: try to fix product advertisement product, reverse withdraw product, store map save via api, currently e2e is used  
-- [ ] E2E_Suite: *** need custom check method  
-- [ ] E2E_Suite: add all email tests  
-- [ ] E2E_Suite: short browser context load in spec file  
-- [ ] E2E_Suite: use global variable instead of env variable
-- [ ] API_Suite: make api suite independent, import: => like dokan pro exists or not, test summary, env setup from e2e suite  
-- [ ] API_Suite: add reverse withdraw and product advertisement on setup tests   
-- [ ] API_Suite: convert admin as vendor to vendor for whole suite  
-- [ ] API_Suite: fix rank math api  
-- [ ] API_Suite: update payloads which causes any php warning [create dokan issue where necessary]
-- [ ] API_Suite: update all tests with product-id, customer-id, to make it faster  
-- [ ] API_Suite: hardcoding admin auth will hinder negative testing : test with invalid user  
-- [ ] API_Suite: Payload generator**  
+-   [ ] Project: add more comments for code readability
+-   [ ] Project: add JSDOC
+-   [ ] Project: add parallelism for both git action & playwright tests
+-   [ ] Project: GRAB CONSOLE ERROR, And PHP ERROR
+-   [ ] TestData: update all image path from project root
+-   [ ] Zip_suite: add zip testing tests
+-   [ ] Selectors: add html tag to every css locator e.g. #id to input#id
+-   [ ] Selectors: shorten selector is all pages e.g. adminSettings = selector.admin.dokan.settings;
+-   [ ] Reporter: add os and browsers in env info
+-   [ ] Reporter: convert testSummary from commonjs module to es-module
+-   [ ] Readme: add readme contribute to e2e_api project
+-   [ ] Readme: rephrase readme
+-   [ ] Readme: fix bookmark issue
+-   [ ] Readme: update readme following readme guideline
+-   [ ] E2E_Suite: List all core tests and setup tests : plugins activated, wp settings, woocommerce settings, dokan modules activated , dokan settings for both e2e and api
+-   [ ] E2E_Suite: Slack integration
+-   [ ] E2E_Suite: Working-directory: ./path/to/tests
+-   [ ] E2E_Suite: Update auth if expired instead of every-time
+-   [ ] E2E_Suite: Report: separate two junit report showing on simple git-action summary
+-   [ ] E2E_Suite: Global setup & teardown can be converted to project setup: no need
+-   [ ] E2E_Suite: Project Configuration: separate everything for local & CI like: global setup, env, playwright config, Configure projects for multiple environments : ci, local,
+-   [ ] E2E_Suite: Fixture: add fixture: separate user role, implement fixture for lite pro issue handle
+-   [ ] E2E_Suite: Test basic auth can be used instead of cookies for authentication , no need to do this
+-   [ ] E2E_Suite: why two pages are opening : fix that
+-   [ ] E2E_Suite: Modal: Close modal if exists, causes issue with other tests where popup blocks next step**\***
+-   [ ] E2E_Suite: add new tests or multiple entry in same test for all search parameter, i.e : by-customer, by-product, but-store,..
+-   [ ] E2E_Suite: insert all date format according to site format , currently hardcoded to default format -> helpers.dateFormatFYJ
+-   [ ] E2E_Suite: try to fix product advertisement product, reverse withdraw product, store map save via api, currently e2e is used
+-   [ ] E2E_Suite:
+// todo: Convert yml step to nodejs script for local development
+// todo: Update goIfNotThere method usages: previous action add some changes but doesn't get reflected due to goIfNotThere
+// todo: HANDLE ERROR: throw new Error('Badge is already published'); : either throw error or just return or convert to playwright error
+// todo: Add modal or sub option to all explotory tesing
+// todo: Add wait for multiple different response on base-page
+// todo: Clear data in beforeAll where necessary
+
+
+
+
+
+
+
+
+
+-   [ ] E2E_Suite: \*\*\* need custom check method
+-   [ ] E2E_Suite: add all email tests
+-   [ ] E2E_Suite: short browser context load in spec file
+-   [ ] E2E_Suite: use global variable instead of env variable
+-   [ ] E2E_Suite: todo: commission tests
+-   [ ] E2E_Suite: todo: add multi store category tests
+-   [ ] E2E_Suite: todo: move to other files: product categories
+-   [ ] E2E_Suite: todo: add multistep product categories test
+-   [ ] E2E_Suite: todo: add product categories settings test
+-   [ ] E2E_Suite: todo: add individual withdraw settings test
+-   [ ] E2E_Suite: todo: add separate files for catalog mode add new tests
+-   [ ] E2E_Suite: todo: add separate files for terms and conditions and add new tests
+-   [ ] E2E_Suite: todo: add multivendor payloads, and update create order for multivendor
+-   [ ] E2E_Suite: todo: add round option for helper methods
+-   [ ] API_Suite: make api suite independent, import: => like dokan pro exists or not, test summary, env setup from e2e suite
+-   [ ] API_Suite: add reverse withdraw and product advertisement on setup tests
+-   [ ] API_Suite: convert admin as vendor to vendor for whole suite
+-   [ ] API_Suite: fix rank math api
+-   [ ] API_Suite: update payloads which causes any php warning [create dokan issue where necessary]
+-   [ ] API_Suite: update all tests with product-id, customer-id, to make it faster
+-   [ ] API_Suite: hardcoding admin auth will hinder negative testing : test with invalid user
+-   [ ] API_Suite: Payload generator\*\*
 
 ### In Progress
 
-
 ### Done ✓
-
-
--- tests/pw/playwright.config.ts
+++ tests/pw/playwright.config.ts
@@ -1,27 +1,47 @@
 import { defineConfig, devices, expect } from '@playwright/test';
 import { customExpect } from '@utils/pwMatchers';
 import 'dotenv/config';
+const { CI, BASE_URL, SLOWMO, ADMIN, ADMIN_PASSWORD } = process.env;
 
 export default defineConfig({
     testDir: './tests/',
     testMatch: /.*\.spec\.ts/,
-    outputDir: 'playwright/test-artifacts/' /* Folder for test artifacts such as screenshots, videos, traces, etc. */,
-    // globalSetup    : './global-setup', /* Path to the global setup file. This file will be required and run before all the tests. */
-    // globalTeardown : './global-teardown', /* Path to the global teardown file. This file will be required and run after all the tests. */
-    globalTimeout: process.env.CI ? 20 * (60 * 1000) : 20 * (60 * 1000) /* Maximum time in milliseconds the whole test suite can run */,
-    maxFailures: process.env.CI ? 20 : 20 /* The maximum number of test failures for the whole test suite run. After reaching this number, testing will stop and exit with an error. */,
-    timeout: process.env.CI ? 45 * 1000 : 45 * 1000 /* Maximum time one test can run for. */,
+    /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+    outputDir: 'playwright/test-artifacts/',
+    /* Path to the global setup file. This file will be required and run before all the tests. */
+    globalSetup: './global-setup',
+    /* Path to the global teardown file. This file will be required and run after all the tests. */
+    // globalTeardown : './global-teardown',
+    /* Maximum time in milliseconds the whole test suite can run */
+    globalTimeout: CI ? 40 * (60 * 1000) : 40 * (60 * 1000),
+    /* The maximum number of test failures for the whole test suite run. After reaching this number, testing will stop and exit with an error. */
+    maxFailures: CI ? 40 : 40,
+    /* Maximum time one test can run for. */
+    timeout: CI ? 45 * 1000 : 45 * 1000,
+    /* Configuration for the expect assertion library */
     expect: {
-        timeout: 10 * 1000 /* Maximum time expect() should wait for the condition to be met.  For example in `await expect(locator).toHaveText();`*/,
-    } /* Configuration for the expect assertion library */,
-    preserveOutput: 'always' /* Whether to preserve test output in the testConfig.outputDir. Defaults to 'always'. */,
-    // fullyParallel  : true, 	/* Run tests in files in parallel */
-    // forbidOnly     : !!process.env.CI, 	/* Fail the build on CI if you accidentally left test-only in the source code. */
-    // repeatEach     : 1, /* The number of times to repeat each test, useful for debugging flaky tests. */
-    retries: process.env.CI ? 1 : 0 /* The maximum number of retry attempts given to failed tests.  */,
-    workers: process.env.CI ? 4 : 1 /* Opt out of parallel tests on CI. */,
+        /* Maximum time expect() should wait for the condition to be met.  For example in `await expect(locator).toHaveText();`*/
+        timeout: 15 * 1000,
+        toHaveScreenshot: {
+            maxDiffPixelRatio: 0.2,
+            maxDiffPixels: 500,
+            threshold: 0.5,
+        },
+    },
+    /* Whether to preserve test output in the testConfig.outputDir. Defaults to 'always'. */
+    preserveOutput: 'always',
+    /* Run tests in files in parallel */
+    // fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test-only in the source code. */
+    // forbidOnly: !!CI,
+    /* The number of times to repeat each test, useful for debugging flaky tests. */
+    repeatEach: CI ? 1 : 0,
+    /* The maximum number of retry attempts given to failed tests.  */
+    retries: CI ? 1 : 0,
+    workers: CI ? 4 : 1 /* Opt out of parallel tests on CI. */,
     reportSlowTests: { max: 3, threshold: 25 } /* Whether to report slow test files. Pass null to disable this feature. */,
-    reporter: process.env.CI
+    /* Configure reporters */
+    reporter: CI
         ? [
               ['github'],
               ['html', { open: 'never', outputFolder: 'playwright-report/html-report' }],
@@ -37,21 +57,52 @@ export default defineConfig({
           ],
     use: {
         ...devices['Desktop Chrome'],
-        acceptDownloads: true /* Whether to automatically download all the attachments. */,
-        actionTimeout: 15 * 1000 /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */,
-        navigationTimeout: 20 * 1000 /* Maximum time each navigation such as 'goto()' can take. */,
-        baseURL: process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:9999' /* Base URL */,
-        // browserName: 'chromium', /* Name of the browser that runs tests. */
-        bypassCSP: true /* Toggles bypassing page's Content-Security-Policy. */,
-        // 	channel: 'chrome', /* Browser distribution channel. */
-        // 	colorScheme: 'dark', /* Emulates 'prefers-colors-scheme' media feature, supported values are 'light', 'dark', 'no-preference' */
-        headless: process.env.CI ? !!process.env.CI : false /* Whether to run tests on headless or non-headless mode */,
-        ignoreHTTPSErrors: true /* Whether to ignore HTTPS errors during navigation. */,
-        trace: 'on' /* Record trace only when retrying a test for the first time. */,
-        screenshot: 'only-on-failure' /* Capture screenshot after each test failure. */,
-        video: 'on-first-retry' /* Record video only when retrying a test for the first time. */,
-        viewport: { width: 1920, height: 1080 } /* Size of viewport */,
-        launchOptions: { slowMo: process.env.SLOWMO ? Number(process.env.SLOWMO) * 1000 : 0 /* whether to slow down test execution by provided seconds */ },
+        /* Whether to automatically download all the attachments. */
+        acceptDownloads: true,
+        /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+        actionTimeout: 25 * 1000,
+        /* Maximum time each navigation such as 'goto()' can take. */
+        navigationTimeout: 30 * 1000,
+        /* Base URL */
+        baseURL: BASE_URL ? BASE_URL : 'http://localhost:9999',
+        /* Name of the browser that runs tests. */
+        // browserName: 'chromium',
+        /* Toggles bypassing page's Content-Security-Policy. */
+        bypassCSP: true,
+        /* Browser distribution channel. */
+        // channel: 'chrome',
+        /* Emulates 'prefers-colors-scheme' media feature, supported values are 'light', 'dark', 'no-preference' */
+        // colorScheme: 'dark' ,
+        /* Whether to run tests on headless or non-headless mode */
+        headless: CI ? !!CI : false,
+        /* Whether to ignore HTTPS errors during navigation. */
+        ignoreHTTPSErrors: true,
+        /* Record trace only when retrying a test for the first time. */
+        // trace: 'on-first-retry',
+        trace: {
+            mode: 'on-first-retry',
+            snapshots: true,
+            screenshots: true,
+            sources: true,
+            attachments: true,
+        },
+        /* Capture screenshot after each test failure. */
+        screenshot: {
+            mode: 'only-on-failure',
+            fullPage: true,
+        },
+        /* Record video only when retrying a test for the first time. */
+        video: 'on-first-retry',
+        /* Size of viewport */
+        viewport: { width: 1420, height: 900 }, // default 1280x720
+        /* whether to slow down test execution by provided seconds */
+        launchOptions: { slowMo: SLOWMO ? Number(SLOWMO) * 1000 : 0 },
+
+        /* api request headers */
+        extraHTTPHeaders: {
+            Accept: '*/*',
+            Authorization: 'Basic ' + Buffer.from(ADMIN + ':' + ADMIN_PASSWORD).toString('base64'),
+        },
     },
 
     /* Configure projects for major browsers */
