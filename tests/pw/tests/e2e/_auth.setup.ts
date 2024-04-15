ce3a9591e622d707d5aa0e83da86312e0d18f803
shashwata <shashwatahalder01@gmail.com>
2024-04-15 13:25:34 +0600

    test: plugin status

--- tests/pw/tests/e2e/_auth.setup.ts
+++ tests/pw/tests/e2e/_auth.setup.ts
@@ -74,9 +74,10 @@ setup.describe('authenticate users & set permalink', () => {
 
     setup('dokan pro enabled or not @lite', async () => {
         let res = await apiUtils.checkPluginsExistence(data.plugin.dokanPro, payloads.adminAuth);
-        helpers.createEnvVar('plugin_status', String(res));
+        helpers.createEnvVar('plugin_exists_status', String(res));
         if (res) {
             res = await apiUtils.pluginsActiveOrNot(data.plugin.dokanPro, payloads.adminAuth);
+            helpers.createEnvVar('plugin_active_status', String(res));
         }
         DOKAN_PRO ? expect(res).toBeTruthy() : expect(res).toBeFalsy();
     });
