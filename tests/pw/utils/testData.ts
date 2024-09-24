f027ea28252875896157c1bdf878d4f3ee7252bc
shashwata <shashwatahalder01@gmail.com>
2024-09-24 21:46:34 +0600

    Fix typos

--- tests/pw/utils/testData.ts
+++ tests/pw/utils/testData.ts
@@ -1013,7 +1013,7 @@ export const data = {
             shippingAddress: 'my-account/edit-address/shipping',
             editAddress: 'my-account/edit-address',
             shippingAddressCheckout: 'wc-ajax=update_order_review',
-            bookedDayBlockes: '?wc-ajax=wc_bookings_find_booked_day_blocks',
+            bookedDayBlocks: '?wc-ajax=wc_bookings_find_booked_day_blocks',
             editAccountCustomer: 'my-account/edit-account',
             becomeVendor: 'my-account/account-migration',
             productDetails: (productName: string) => `product/${productName}`,
