aa81138ff1eeae4de9e3f424ebaf96cf77f55b02
shashwata <shashwatahalder01@gmail.com>
2024-12-17 16:42:28 +0600

    Add sharding for parallel

--- tests/pw/e2e.config.ts
+++ tests/pw/e2e.config.ts
@@ -49,8 +49,9 @@ export default defineConfig({
     /* Configure reporters */
     reporter: CI
         ? [
+              ['blob'],
               //   ['github'],
-              ['html', { open: 'never', outputFolder: 'playwright-report/e2e/html-report' }],
+              //   ['html', { open: 'never', outputFolder: 'playwright-report/e2e/html-report' }],
               //   ['junit', { outputFile: 'playwright-report/e2e/junit-report/e2e-results.xml' }],
               ['list', { printSteps: true }],
               ['./utils/summaryReporter.ts', { outputFile: 'playwright-report/e2e/summary-report/results.json' }],
